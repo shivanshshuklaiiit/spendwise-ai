bfd07ef0c521c4acb8aa563e7a74217aa536219e
Delta Student <shivanshshuklatech@gmail.com>
2025-03-13 14:20:57 +0530

    Made the Landing Page and Dashboard page database

---
+++ app/(main)/dashboard/page.jsx
@@ -0,0 +1,33 @@
+
+import CreateAccountDrawer from "@/components/create-account-drawer";
+import React from "react"
+import { Card, CardContent } from "@/components/ui/card";
+import { Plus } from "lucide-react";
+import { getUserAccounts } from "@/actions/dashboard";
+import {AccountCard} from "./_components/account-card";
+
+async function DashboardPage() {
+  const accounts = await getUserAccounts();
+  return (
+    <div className="px-5">
+      <div>
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+        <CreateAccountDrawer>
+          <Card className="hover:shadow-md transition-shadow cursor-pointer border-dashed">
+            <CardContent className="flex flex-col items-center justify-center text-muted-foreground h-full pt-5">
+              <Plus className="h-10 w-10 mb-2" />
+              <p className="text-sm font-medium">Add New Account</p>
+            </CardContent>
+          </Card>
+        </CreateAccountDrawer>
+        {accounts.length > 0 &&
+          accounts?.map((account) => (
+            <AccountCard key={account.id} account={account} />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default DashboardPage;
